929aa578552bec906682b9f80644f2f88cf2b346
LegitAhmad <a.s20061111@gmail.com>
2026-09-08 23:36:12 +0500

    remove bin files

--- apps/mobile/android/app/.cxx/Debug/413r5g4y/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/home/legitahmad/.gradle/caches/9.4.1/transforms/8a3e8a9038ec4194d90c2409e612a2a9/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/legitahmad/.gradle/caches/9.4.1/transforms/8a3e8a9038ec4194d90c2409e612a2a9/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- apps/mobile/android/app/.cxx/Debug/6e3g673n/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "/home/legitahmad/.gradle/caches/9.3.1/transforms/ac804f2b95b14e41a04ff0203d1be5e7/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/legitahmad/.gradle/caches/9.3.1/transforms/ac804f2b95b14e41a04ff0203d1be5e7/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
